daeba578e47fa361166a8e850e655617a800b79a
Martin Webster <mwebster1973@gmail.com>
2024-11-28 09:03:41 +0000

    fix: remove files

--- src/day1.ts
+++
@@ -1,4 +0,0 @@
-export const part1 = (num: number) => {
-    console.log(num)
-    return num;
-}
--- src/day2.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, describe, expect } from 'vitest'
-import { part1 } from './day1'
-
-describe('exemple ', () => {
-    test('example ', () => {
-        expect(part1(1)).toBe(1)
-    })
-})
