d912badd0ecec3dd453f05c043acd29fa6cad163
Martin Webster <mwebster1973@gmail.com>
2024-12-01 07:45:44 +0000

    Tidy up tests

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -8,46 +8,38 @@ extensions.apply();
 
 describe('day 1 - part1', () => {
 
+    const sampleData = [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3",
+    ]
+
     test('diff', () => {
         expect(diff(3, 4)).toBe(1);
         expect(diff(9, 3)).toBe(6);
     })
 
     test('sample', () => {
-        const data = [
-            "3   4",
-            "4   3",
-            "2   5",
-            "1   3",
-            "3   9",
-            "3   3",
-        ]
-        expect(extract(data[0])).toStrictEqual([3, 4]);
-        expect(pairs(data)).toBe(11);
+
+        expect(extract(sampleData[0])).toStrictEqual([3, 4]);
+        expect(pairs(sampleData)).toBe(11);
     })
 
     test('part1', () => {
         const data = readTestData('./src/day1/day1.txt');
-        const result = pairs(data);
-        expect(result).toBe(2057374);
+        expect(pairs(data)).toBe(2057374);
     })
 
 
-    test('sample', () => {
-        const data = [
-            "3   4",
-            "4   3",
-            "2   5",
-            "1   3",
-            "3   9",
-            "3   3",
-        ]
-        expect(similarity(data)).toBe(31);
+    test('sample - part 2', () => {
+        expect(similarity(sampleData)).toBe(31);
     })
 
     test('part2', () => {
         const data = readTestData('./src/day1/day1.txt');
-        const result = similarity(data);
-        expect(result).toBe(23177084);
+        expect(similarity(data)).toBe(23177084);
     })
 })
