59bda8a0a15c9df0b44f364f0bae948b07e57655
Martin Webster <mwebster1973@gmail.com>
2024-12-01 07:41:17 +0000

    fix: first version

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -2,18 +2,52 @@
 import * as extensions from '../utility/extensions';
 import { test, describe, expect } from 'vitest'
 import { readTestData } from '../utility/fileHelper';
-import { exampleFunction } from './day1';
+import { diff, extract, pairs, similarity } from './day1';
 
 extensions.apply();
 
 describe('day 1 - part1', () => {
+
+    test('diff', () => {
+        expect(diff(3, 4)).toBe(1);
+        expect(diff(9, 3)).toBe(6);
+    })
+
     test('sample', () => {
-        expect(exampleFunction('1')).toBe(1);
+        const data = [
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3",
+        ]
+        expect(extract(data[0])).toStrictEqual([3, 4]);
+        expect(pairs(data)).toBe(11);
     })
 
     test('part1', () => {
         const data = readTestData('./src/day1/day1.txt');
-        const sum = data.map(line => exampleFunction(line)).sum();
-        expect(sum).toBe(6);
+        const result = pairs(data);
+        expect(result).toBe(2057374);
+    })
+
+
+    test('sample', () => {
+        const data = [
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3",
+        ]
+        expect(similarity(data)).toBe(31);
+    })
+
+    test('part2', () => {
+        const data = readTestData('./src/day1/day1.txt');
+        const result = similarity(data);
+        expect(result).toBe(23177084);
     })
 })
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,3 +1,35 @@
-export const exampleFunction = (line: string): number | undefined => {
-    return Number(line);
+export const diff = (locationId1: number, locationId2: number): number => {
+    return Math.abs(locationId2 - locationId1);
+}
+
+export const extract = (line: String): number[] => {
+    return line.split("   ").map(it => Number(it));
+}
+
+export const pairs = (lines: String[]): number => {
+
+    const list1 = lines.map(line => extract(line)[0]).sort((a, b) => a - b)
+    const list2 = lines.map(line => extract(line)[1]).sort((a, b) => a - b)
+
+    var total = 0;
+
+    for (let index = 0; index < list1.length; index++) {
+        total += diff(list1[index], list2[index]);
+    }
+    return total;
+}
+
+export const similarity = (lines: String[]): number => {
+
+    const list1 = lines.map(line => extract(line)[0])
+    const list2 = lines.map(line => extract(line)[1])
+
+    var total = 0;
+
+    for (let index = 0; index < list1.length; index++) {
+        const value = list1[index];
+        var count = list2.filter(it => it == value);
+        total += value * count.length;
+    }
+    return total;
 }
