bc9dc61f31abc598fb6c7387c76422c330a111b9
Martin Webster <mwebster1973@gmail.com>
2024-12-02 07:04:12 +0000

    part 2

--- src/day2/day2.test.ts
+++ src/day2/day2.test.ts
@@ -2,7 +2,7 @@
 import * as extensions from '../utility/extensions';
 import { test, describe, expect } from 'vitest'
 import { readTestData } from '../utility/fileHelper';
-import { isSafe, safeCount } from './day2';
+import { isSafe, isSafe2, safeCount, safeCount2 } from './day2';
 
 extensions.apply();
 
@@ -18,33 +18,37 @@ describe('day 1 - part1', () => {
     ]
 
     test('sample', () => {
-        // expect(sampleData[0].toNumbers()).toStrictEqual([7, 6, 4, 2, 1]);
-        // expect(isSafe(sampleData[0])).toBe(true);
-        // expect(isSafe(sampleData[1])).toBe(false);
-        // expect(isSafe(sampleData[2])).toBe(false);
-        // expect(isSafe(sampleData[3])).toBe(false);
-        // expect(isSafe(sampleData[4])).toBe(false);
-        // expect(isSafe(sampleData[5])).toBe(true);
-        // expect(safeCount(sampleData)).toBe(2);
-        // expect(isSafe("7 6 4 2 1 0")).toBe(true);
-        // expect(isSafe("4 4 5 6 7")).toBe(false);
-        // expect(isSafe("52 52 51 49 47 45 42 38")).toBe(false);
-        expect(isSafe("20 18 19 22 25 30")).toBe(false)
+        expect(sampleData[0].toNumbers()).toStrictEqual([7, 6, 4, 2, 1]);
+        expect(isSafe(sampleData[0].toNumbers())).toBe(true);
+        expect(isSafe(sampleData[1].toNumbers())).toBe(false);
+        expect(isSafe(sampleData[2].toNumbers())).toBe(false);
+        expect(isSafe(sampleData[3].toNumbers())).toBe(false);
+        expect(isSafe(sampleData[4].toNumbers())).toBe(false);
+        expect(isSafe(sampleData[5].toNumbers())).toBe(true);
+        expect(safeCount(sampleData)).toBe(2);
     })
 
     test('part1', () => {
         const data = readTestData('./src/day2/input.txt');
         // 346 too high // 345
-        expect(safeCount(data)).toBe(2);
+        expect(safeCount(data)).toBe(326);
 
     })
 
-    // test('sample - part 2', () => {
-    //     expect(similarity(sampleData)).toBe(31);
-    // })
+    test('sample 2', () => {
+        expect(sampleData[0].toNumbers()).toStrictEqual([7, 6, 4, 2, 1]);
+        expect(isSafe2(sampleData[0].toNumbers())).toBe(true);
+        expect(isSafe2(sampleData[1].toNumbers())).toBe(false);
+        expect(isSafe2(sampleData[2].toNumbers())).toBe(false);
+        expect(isSafe2(sampleData[3].toNumbers())).toBe(true);
+        expect(isSafe2(sampleData[4].toNumbers())).toBe(true);
+        expect(isSafe2(sampleData[5].toNumbers())).toBe(true);
+        expect(safeCount2(sampleData)).toBe(4);
+    })
 
-    // test('part2', () => {
-    //     const data = readTestData('./src/day2/input.txt');
-    //     expect(similarity(data)).toBe(23177084);
-    // })
+    test('part2', () => {
+        const data = readTestData('./src/day2/input.txt');
+        expect(safeCount2(data)).toBe(381);
+
+    })
 })
--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,11 +1,13 @@
 
-export const isSafe = (line: String): boolean => {
+export const isSafe = (numbers: number[]): boolean => {
     var up = true;
-    const numbers = line.toNumbers();
     if (numbers[0] > numbers[1]) {
         up = false;
     } else {
         up = true;
+    }
+    if (numbers.length == 1) {
+        return false;
     }
 
     for (let index = 0; index < numbers.length - 1; index++) {
@@ -15,22 +17,38 @@ export const isSafe = (line: String): boolean => {
             return false;
         }
 
-        console.log(current, next)
         var value = next - current;
-        if (!up) {
-            value = Math.abs(value)
-        }
 
-        if (value > 3 || value <= 0) {
-            return false
+        if (up) {
+            if (value > 3 || value <= 0) {
+                return false
+            }
+        } else {
+            if (value < -3 || value >= 0) {
+                return false
+            }
         }
     }
     return true;
-
 }
 
 // part 1
 export const safeCount = (lines: string[]): number => {
-    return lines.filter(isSafe).length;
+    return lines.map(it => it.toNumbers()).filter(isSafe).length;
 }
 
+// part 2
+export const isSafe2 = (numbers: number[]): boolean => {
+
+    for (let index = 0; index < numbers.length; index++) {
+        var newNumbers = [...numbers.slice(0, index), ...numbers.slice(index + 1)]
+        if (isSafe(newNumbers)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+export const safeCount2 = (lines: string[]): number => {
+    return lines.map(it => it.toNumbers()).filter(isSafe2).length;
+}
