4b3c5da856d48b5b169e9a3aa88184657ceb542f
Martin Webster <mwebster1973@gmail.com>
2024-12-04 07:13:35 +0000

    initial day 4

--- src/day4/day4.test.ts
+++ src/day4/day4.test.ts
@@ -2,22 +2,58 @@
 import * as extensions from '../utility/extensions';
 import { test, describe, expect } from 'vitest'
 import { readTestData } from '../utility/fileHelper';
+import { countMasX, countX, countXmas, isLeft, isRight, isUp } from './day4';
 
 extensions.apply();
 
 describe('day 4', () => {
 
-    test('sample', () => {
+    test('sample 1', () => {
+      const data = [
+          "..X...",
+          ".SAMX.",
+          ".A..A.",
+          "XMAS.S",
+          ".X....",
+      ]
+      expect(isLeft(data, 2, 1)).toBe(false);
+      expect(isLeft(data, 4, 1)).toBe(true);
+      expect(isLeft(data, 5, 1)).toBe(false);
+
+      expect(isRight(data, 4, 1)).toBe(false);
+      expect(isRight(data, 0, 3)).toBe(true);
+      expect(isRight(data, 1, 3)).toBe(false);
+
+      expect(isUp(data, 1, 4)).toBe(true);
+      expect(countXmas(data)).toBe(4)
+    })
+
+    test('sample 2', () => {
+      const data = readTestData('./src/day4/input.sample.txt');
+      expect(countXmas(data)).toBe(18)
     })
 
     test('part1', () => {
          const data = readTestData('./src/day4/input.txt');
+         expect(countXmas(data)).toBe(2551)
     })
 
-    test('sample 2', () => {
+    test('sample part 2', () => {
+      const data = [
+        "M.S",
+        ".A.",
+        "M.S"]
+        expect(countX(data, 1, 1)).toBe(1)
+        expect(countMasX(data)).toBe(1)
       })
+
+    test('sample part 22', () => {
+      const data = readTestData('./src/day4/input.sample.txt');
+      expect(countMasX(data)).toBe(9)
+    })
 
      test('part2', () => {
         const data = readTestData('./src/day4/input.txt');
+        expect(countMasX(data)).toBe(1985)
    })
 })
--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -1,4 +1,155 @@
-export const buildMul = (instruction: string): number[] =>{
-    return [ Number(instruction.substringAfter("(").substringBefore(",")), Number(instruction.substringAfter(",").substringBefore(")")) ];
+
+const XMAS = "XMAS"
+
+const getValue = (rows: String[], xPos: number, yPos: number) : string | undefined =>{
+    if (rows[yPos] === undefined){
+        return undefined;
+    }
+    return rows[yPos].charAt(xPos); 
 }
 
+export const isLeft = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if ( getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentX--;
+    }
+    return true;
+}
+
+export const isRight = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentX++;
+    }
+    return true;
+}
+
+export const isUp = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentY--;
+    }
+    return true;
+}
+
+export const isDown = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentY++;
+    }
+    return true;
+}
+
+export const isUpLeft = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentX--;
+        currentY--;
+    }
+    return true;
+}
+
+export const isUpRight = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentX++;
+        currentY--;
+    }
+    return true;
+}
+
+export const isDownLeft = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentX--;
+        currentY++;
+    }
+    return true;
+}
+
+export const isDownRight = (rows: String[], xPos: number, yPos: number, word: String = XMAS): boolean =>{
+    var currentX = xPos;
+    var currentY = yPos;
+    for (let index = 0; index < word.length; index++) {
+        if (getValue(rows, currentX, currentY)!== word.charAt(index)){
+            return false;
+        }
+        currentX++;
+        currentY++;
+    }
+    return true;
+}
+
+export const countPresent = (rows: String[], xPos: number, yPos: number): number => {
+    var count = 0;
+    if (isDown(rows, xPos, yPos)) count++
+    if (isDownLeft(rows, xPos, yPos)) count++
+    if (isDownRight(rows, xPos, yPos)) count++
+    if (isLeft(rows, xPos, yPos)) count++
+    if (isRight(rows, xPos, yPos)) count++
+    if (isUp(rows, xPos, yPos)) count++
+    if (isUpLeft(rows, xPos, yPos)) count++
+    if (isUpRight(rows, xPos, yPos)) count++
+    return count;
+}
+
+export const countXmas =  (rows: String[]): number => {
+    var total = 0;
+    for (let yPos = 0; yPos < rows.length; yPos++) {
+        for (let xPos = 0; xPos < rows[0].length; xPos++) {
+            total = total + countPresent(rows, xPos, yPos)
+        }
+    }
+    return total;
+}
+
+export const countX = (rows: String[], xPos: number, yPos: number): number => {
+    var count = 0;
+    const word = "MAS"
+    if (isDownRight(rows, xPos-1, yPos-1, word)) count++
+    if (isDownLeft(rows, xPos+1, yPos-1, word)) count++
+    if (isUpLeft(rows, xPos+1, yPos+1, word)) count++
+    if (isUpRight(rows, xPos-1, yPos+1, word)) count++
+    if (count===2){
+        return 1;
+    }
+    return 0;
+}
+
+export const countMasX =  (rows: String[]): number => {
+    var total = 0;
+    for (let yPos = 0; yPos < rows.length; yPos++) {
+        for (let xPos = 0; xPos < rows[0].length; xPos++) {
+            total = total + countX(rows, xPos, yPos)
+        }
+    }
+    return total;
+}
