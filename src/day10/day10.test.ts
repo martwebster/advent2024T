158e67e0ebc3f6000c91b4bba50c7be7efafb559
Martin Webster <mwebster1973@gmail.com>
2024-12-09 22:37:35 +0000

    prepare for day 10

---
+++ src/day10/day10.test.ts
@@ -0,0 +1,20 @@
+
+import '../utility/extensions';
+import { test, describe, expect } from 'vitest'
+
+
+
+
+describe('day 9', () => {
+
+  test('sample', () => {
+  })
+  
+  test('part1', () => {
+  })
+  
+  test('part2 - sample 2', () => {
+  })
+
+  test('part2', () => {  })
+})
