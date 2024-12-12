747468fd05270e93cfbc099e4048e1d547f8dc2d
Martin Webster <mwebster1973@gmail.com>
2024-12-12 14:51:19 +0000

    feat(day 12): add sample input files and implement region building and evaluation logic; include corresponding tests

--- src/day12/day12.test.ts
+++ src/day12/day12.test.ts
@@ -1,24 +1,121 @@
 
 import '../utility/extensions';
 import { test, describe, expect } from 'vitest'
-import {  sum} from './day12';
+import {  buildRegions, findRegion, getDiscountPrice, getPrice, horizontal, horizontalBottom, horizontalTop, sides, vertical, verticalLeft, verticalRight} from './day12';
 import { readTestData } from '../utility/fileHelper';
 
 const data = readTestData('./src/day10/input.txt');
 describe('day 12', () => {
 
 
-  test('sample 2', () => {
-    var numbers = sum()
+  test('sample 1', () => {
+    var garden = ["AAAA",
+    "BBCD",
+    "BBCC",
+    "EEEC"]
 
-    expect(numbers).toBe(22)
+    // create a set of regions
+    // start from top lef, and flood fill
+    // adding to region
+
+    var region = findRegion(garden, {x: 0 ,y:0})
+
+    expect(region.plant).toBe("A")
+    expect(region.positions.length).toBe(4)
+    expect(region.perimeter).toBe(10)
+
+    var region = findRegion(garden, {x: 0 ,y:1})
+
+    expect(region.plant).toBe("B")
+    expect(region.positions.length).toBe(4)
+    expect(region.perimeter).toBe(8)
+    
+    var region = findRegion(garden, {x: 2 ,y:1})
+
+    expect(region.plant).toBe("C")
+    expect(region.positions.length).toBe(4)
+    expect(region.perimeter).toBe(10)
+
+    var region = findRegion(garden, {x: 3 ,y:1})
+
+    expect(region.plant).toBe("D")
+    expect(region.positions.length).toBe(1)
+    expect(region.perimeter).toBe(4)
+
+    var region = findRegion(garden, {x: 0 ,y:3})
+
+    expect(region.plant).toBe("E")
+    expect(region.positions.length).toBe(3)
+    expect(region.perimeter).toBe(8)
+
+    var regions = buildRegions(garden);
+    expect (regions.length).toBe(5)
+    expect (getPrice(regions)).toBe(140)
+  })
+
+  test('sample 2', () => {
+    const garden = [
+    "OOOOO",
+    "OXOXO",
+    "OOOOO",
+    "OXOXO",
+    "OOOOO"]
+    var regions = buildRegions(garden);
+    expect( regions.length).toBe(5)
+    expect (regions[0].plant).toBe("O")
+    expect(regions[0].positions.length).toBe(21)
+    expect(regions[0].perimeter).toBe(36)
+
+    expect ( getPrice(regions)).toBe(772)
+    // part 2
+    expect ( sides(regions[0])).toBe(20)
+    expect ( getDiscountPrice(regions)).toBe(436)
+
   })
 
   
-  test('part 1', () => {
-    const data = readTestData('./src/day10/input.txt');
-
+  test('big sample', () => {
+    const data = readTestData('./src/day12/input.sample.txt');
+    var regions = buildRegions(data);
+    var r = regions.find(it => it.plant=="R")!
+    expect (r.perimeter).toBe(18)
+    expect (r.positions.length).toBe(12)
+    
+    expect (getPrice(regions)).toBe(1930)
+    // part 2
+    expect (getDiscountPrice(regions)).toBe(1206)
+    
   });
+
+  test('part 1', () => {
+    const data = readTestData('./src/day12/input.txt');
+    var regions = buildRegions(data);
+    expect (getPrice(regions)).toBe(1465968 )
+
+    // part 2
+    expect (getDiscountPrice(regions)).toBe(897702 )
+  });
+
+  test('part 2 - sample', () => {
+    var garden = ["AAAA",
+      "BBCD",
+      "BBCC",
+      "EEEC"]
+    var regions = buildRegions(garden);
+
+    expect (sides(regions[0])).toBe(4)
+    expect (sides(regions[2])).toBe(8)
+    expect (getDiscountPrice(regions)).toBe(80)
+    
+  });
+
+  test('sample 2', () => {
+    const data = readTestData('./src/day12/input.sample2.txt');
+    var regions = buildRegions(data);
+    
+    expect (getDiscountPrice(regions)).toBe(236)
+  });
+
 
  
 })
--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -1,3 +1,213 @@
-export const sum = (): number =>{
-    return 1;
+export const buildRegions = (garden: string[]):  Region[]=>{
+    
+    var regions : Region[] =[]
+    for (let yPos = 0; yPos < garden.length; yPos++) {
+       for (let xPos = 0; xPos < garden[yPos].length; xPos++) {
+        var currentPos = {
+            x: xPos,
+            y: yPos
+        }
+        const found = regions.map (it => it.positions)
+            .flat()
+            .find (it => it.x == currentPos.x && it.y==currentPos.y) 
+        if (!found){
+            regions.push(findRegion(garden, currentPos))
+        }
+       } 
+    }
+    return regions;
+}
+
+const getPlant = (garden: string[], pos : Pos) :string| undefined =>{
+    
+    if (pos.y<0 ||  pos.y >= garden.length){
+        return undefined
+    }
+    return garden[pos.y][pos.x]
+}
+
+export interface Region{
+    positions: Pos[],
+    perimeter: number,
+    plant : string,
+    horizPerimTop : Pos[], 
+    horizPerimBottom : Pos[],
+    verticalPerimLeft : Pos[],
+    verticalPerimRight : Pos[]
+}
+
+export const flood = (garden: string[], currentPos: Pos, region: Region) =>{
+    // move up
+    var up : Pos = {
+        x: currentPos.x,
+        y: currentPos.y-1
+    }
+    var exists = region.positions.find( it => it.x == up.x && it.y == up.y)
+    if (!exists){
+        var nextPlant =  getPlant(garden, up)
+        if (nextPlant == undefined || nextPlant !== region.plant){
+            region.perimeter = region.perimeter+1
+            region.horizPerimTop.push(currentPos)
+        } else{ 
+            region.positions.push(up)
+            flood(garden, up, region)
+        }
+    }
+    // move down
+    var down : Pos = {
+        x: currentPos.x,
+        y: currentPos.y+1
+    }
+    var exists = region.positions.find( it => it.x == down.x && it.y == down.y)
+    if (!exists){
+        var nextPlant =  getPlant(garden, down)
+        if (nextPlant == undefined || nextPlant !== region.plant){
+            region.perimeter = region.perimeter+1
+            region.horizPerimBottom.push(currentPos)
+        } else{ 
+            region.positions.push(down)
+            flood(garden, down, region)
+        }
+    }
+
+
+    // move left
+    var left : Pos = {
+        x: currentPos.x-1,
+        y: currentPos.y
+    }
+    var exists = region.positions.find( it => it.x == left.x && it.y == left.y)
+    if (!exists){
+        var nextPlant =  getPlant(garden, left)
+        if (nextPlant == undefined || nextPlant !== region.plant){
+            region.perimeter = region.perimeter+1
+            region.verticalPerimLeft.push(currentPos)
+        } else{ 
+            region.positions.push(left)
+            flood(garden, left, region)
+        }
+    }
+
+    // move right
+    var right : Pos = {
+        x: currentPos.x+1,
+        y: currentPos.y
+    }
+    var exists = region.positions.find( it => it.x == right.x && it.y == right.y)
+    if (exists){
+        return;
+    }
+    var nextPlant =  getPlant(garden, right)
+    if (nextPlant == undefined || nextPlant !== region.plant){
+        region.perimeter = region.perimeter+1
+        region.verticalPerimRight.push(currentPos)
+    } else{ 
+        region.positions.push(right)
+        flood(garden, right, region)
+    }
+}
+
+export const findRegion = (garden: string[], currentPos : Pos) : Region =>{
+    const plant = getPlant(garden, currentPos)!
+    // starting from this position
+    var region : Region = {
+        positions : [currentPos],
+        perimeter: 0,
+        plant, 
+        horizPerimBottom: [],
+        horizPerimTop: [],
+        verticalPerimLeft : [],
+        verticalPerimRight : [],
+    }
+    flood(garden, currentPos, region)
+
+    return region;
+}
+
+export const getPrice = (regions: Region[]): number =>{
+    return regions.sumOf (it => it.positions.length * it.perimeter)
+}
+
+// part 2
+export const sides =  (region : Region): number =>{
+    return horizontal(region.horizPerimBottom)+
+           horizontal(region.horizPerimTop) +
+           vertical(region.verticalPerimLeft) +
+           vertical(region.verticalPerimRight)
+}
+
+export const horizontal = (pos: Pos[]): number =>{
+    var minX = pos.minOf ( it => it.x)
+    var maxX = pos.maxOf ( it => it.x)
+    
+    var minY = pos.minOf ( it => it.y)
+    var maxY = pos.maxOf ( it => it.y)
+    
+    var count = 0;
+    var current = false
+    for (let yPos = minY; yPos <= maxY; yPos++) {
+       for (let xPos = minX; xPos <= maxX; xPos++) {
+            var found = pos.find (it => it.x == xPos && it.y == yPos)
+            if (!found && current){
+               // gap
+               count++
+               current = false
+            } else if (found && !current){
+                current = true
+            }
+        }
+        // end of the row
+        if (current){
+            count++
+            current = false
+        }
+    }
+    return count
+}
+
+
+
+
+export const verticalRight = (region: Region): number =>{
+    return vertical(region.verticalPerimRight)
+}
+
+export const verticalLeft = (region: Region): number =>{
+    return vertical(region.verticalPerimLeft)
+}
+
+export const vertical = (pos: Pos[]): number =>{
+    var minX = pos.minOf ( it => it.x)
+    var maxX = pos.maxOf ( it => it.x)
+    
+    var minY = pos.minOf ( it => it.y)
+    var maxY = pos.maxOf ( it => it.y)
+    
+    var count = 0;
+    var current = false
+    for (let xPos = minX; xPos <= maxX; xPos++) {
+        for (let yPos = minY; yPos <= maxY; yPos++) {
+            var found = pos.find (it => it.x == xPos && it.y == yPos)
+            if (!found && current){
+               // gap
+               count++
+               current = false
+            } else if (found && !current){
+               current = true
+            }
+        }
+        // end of the column
+        if (current){
+            count++
+            current = false
+        }
+    }
+    return count
+
+}
+
+
+
+export const getDiscountPrice = (regions: Region[]): number =>{
+    return regions.sumOf (it => it.positions.length * sides(it))
 }
