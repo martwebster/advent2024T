9503219259828ed86af6b1faddbee4af17451498
Martin Webster <mwebster1973@gmail.com>
2024-12-23 11:38:02 +0000

    refactor(day 23): rename linkNodes to populateNodes and simplify node linking logic

--- src/day23/day23.test.ts
+++ src/day23/day23.test.ts
@@ -2,43 +2,32 @@
 import '../utility/extensions';
 import { test, describe, expect } from 'vitest'
 import { readTestData } from '../utility/fileHelper';
-import { getPart1, getPassword, linkNodes } from './day23';
+import { getPart1, getPassword, populateNodes } from './day23';
 
 
 describe('day 23', () => {
    test ('Sample', () =>{
-      const nodes = linkNodes(readTestData('./src/day23/input.sample.txt'))
+      const nodes = populateNodes(readTestData('./src/day23/input.sample.txt'))
       expect (nodes.length).toBe(16)
       expect (getPart1(nodes)).toBe(7) 
    })
 
    test ('Part 1', () =>{
       const data = readTestData('./src/day23/input.txt')
-      const nodes = linkNodes(data)
+      const nodes = populateNodes(data)
       expect (nodes.length).toBe(520)
       expect (getPart1(nodes)).toBe(1240)
    })
 
    test ('Sample - Part 2', () =>{
-      const data = linkNodes(readTestData('./src/day23/input.sample.txt'))
+      const data = populateNodes(readTestData('./src/day23/input.sample.txt'))
       expect(getPassword(data)).toBe("co,de,ka,ta")
    })
 
    test ('Part 2', () =>{
       const data = readTestData('./src/day23/input.txt')
-      const nodes = linkNodes(data)
+      const nodes = populateNodes(data)
       expect(getPassword(nodes)).toBe("am,aq,by,ge,gf,ie,mr,mt,rw,sn,te,yi,zb")
    })
-
-   // Part 2
-   // form parties.
-
-   // somone can only join a party, if they have a link with everyone else in that party
-
-   // first person, joins a party
-   // if second person, is linked to first
-   // they can join party.
-   // if not they start there own party.
-   // next person.
 
 })
--- src/day23/day23.ts
+++ src/day23/day23.ts
@@ -1,34 +1,37 @@
-export class Node {
+export interface Node {
     id: string;
-    links : Node[] = []
-    constructor(id: string){
-        this.id = id;
-    }
+    links : Node[]
 }
 
-export const linkNodes= ( data: string[]): Node[] =>{
+export const findOrAdd = (nodes: Node[], id: string): Node =>{
+    var node = nodes.find (it => it.id == id)
+    if (node == undefined){
+        node = {
+            id: id,
+            links: []
+        }
+        nodes.push(node)
+    }
+    return node
+}
+
+export const populateNodes= ( data: string[]): Node[] =>{
     const nodes : Node[] = []
     data.forEach (it => {
-        const id1 = it.substringBefore("-")
-        var node1 = nodes.find (it => it.id == id1)
-        if (node1 == undefined){
-            node1 = new Node(id1);
-            nodes.push(node1)
-        }
-
-        const id2 = it.substringAfter("-")
-        var node2 = nodes.find (it => it.id == id2)
-        if (node2 == undefined){
-            node2 = new Node(id2);
-            nodes.push(node2)
-        }
-
+        const node1 = findOrAdd(nodes, it.substringBefore("-"));
+        const node2 = findOrAdd(nodes, it.substringAfter("-"));
+        
         node1.links.push(node2)
         node2.links.push(node1)
     })   
     return nodes
 }
 
+/**
+ * Count the number of nodes that have formed parties of 3.
+ * @param nodes 
+ * @returns count
+ */
 export const getPart1 = (nodes: Node[]) : number => {
     var links : Array<Set<string>> = []
     var ts = nodes.filter( it => it.id.startsWith("t"))
@@ -55,17 +58,17 @@ export const getPassword = (nodes: Node[]) : string =>{
     nodes.forEach (node =>{
         var joined = false;
 
-        parties.forEach (party =>{
-            if (!joined){
-                var notLinked = party.some (guest => !node.links.map(it => it.id).includes(guest))
-                if (!notLinked){
-                    joined = true;
-                    party.push(node.id)
-                }
+        // see if you can join an existing party (only if know everyone!. What a clique)
+        for (const party of parties) {
+            // scan the room, see if youi know anyone.. i.e no stragers.
+            var strangers = party.some (guest => !node.links.map(it => it.id).includes(guest))
+            if (!strangers){ // if there are no strangers, join the party.
+                joined = true;
+                party.push(node.id)
+                break;
             }
-        })
-
-        // if not joined, add to new party
+        }
+        // if you didn't find a party to join, start a new one. Saddo!. Make friends.
         if (!joined){
             parties.push( [node.id])
         }
@@ -76,7 +79,7 @@ export const getPassword = (nodes: Node[]) : string =>{
         const currentParty = parties[partyId];
         for (let guestId = currentParty.length-1; guestId >=0 ; guestId--) {
             const guest = currentParty[guestId];
-            const nextParty = getNextParty(nodes, guest, parties.slice(partyId+1))
+            const nextParty = findACoolParty(nodes, guest, parties.slice(partyId+1))
             if (nextParty!= undefined){
                 nextParty.push(guest)
                 currentParty.removeAtIndex(guestId)
@@ -87,20 +90,24 @@ export const getPassword = (nodes: Node[]) : string =>{
     var result = parties[0].sort();
 
     return result.join(",");
-
 }
 
-const getNextParty = (nodes: Node[],guest: string, parties: string[][]): string[]| undefined =>{
+/**
+ * You've started a party, but there is another party... elsewhere, 
+ * and you are stuck talking to someone about your favourite stretcher of the motorway.
+ * This function looks to see if there is another party you can gatecrash
+ * @param nodes 
+ * @param guest 
+ * @param otherParties 
+ * @returns a cool party to join
+ */
+const findACoolParty = (nodes: Node[],guest: string, otherParties: string[][]): string[]| undefined =>{
     var guestNodeLinks = nodes.find (it => it.id == guest)?.links.map (it => it.id)
-    var foundParty : string[]| undefined;
-    parties.forEach (party =>{
-        if(!foundParty){
-            const ps = party.find (it => !guestNodeLinks?.includes(it))
-            if (ps == undefined){
-                foundParty = party
-            }
-
+    for (const party of otherParties) {
+        const strangers = party.some (it => !guestNodeLinks?.includes(it))
+        if (!strangers){
+            return party // we know everyone in this party, usallly the kitchen
         }
-    })
-    return foundParty
+    }
+    return undefined
 }
