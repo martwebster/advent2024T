0aad16e004ae5b41c53d54f1af7702140bebec9b
Martin Webster <mwebster1973@gmail.com>
2024-12-30 09:41:35 +0000

    Day 20 - Agian

--- src/day20/day20.test.ts
+++ src/day20/day20.test.ts
@@ -1,29 +1,53 @@
+import "../utility/extensions"
+import { test, describe, expect } from "vitest"
+import { readTestData } from "../utility/fileHelper"
+import { createTrack, displayTrack, getFastest, getSavings, moveToEnd, recordBaseline } from "./day20"
 
-import '../utility/extensions';
-import { test, describe, expect } from 'vitest'
-import { readTestData } from '../utility/fileHelper';
-import { createTrack, getCounts, getInnerTrack, moveToEnd } from './day20';
+describe("day 20", () => {
+   test("Sample 1", () => {
+      const data = readTestData("./src/day20/input.sample.txt")
+      const track = createTrack(data)
+      const baseline = moveToEnd(track, 0)[0]
 
-describe('day 20', () => {
-  
-   test('Sample 1', () => {
-     const data = readTestData('./src/day20/input.sample.txt');
-     const track = createTrack(data);
-     const inner = getInnerTrack(track)
-     expect(moveToEnd(track)).toBe(84)
-     expect(inner.length).toBe(84)
-     getCounts(track, inner)
+      recordBaseline(track, baseline.visited)
+
+      expect(getFastest(track,0)).toBe(84)
+      expect(getFastest(track,1)).toBe(20)
+      expect(getFastest(track,3)).toBe(12)
+
+      expect(getSavings(track,1,1)).toBe(44)
    })
 
-    test('Part 1', () => {
-      const data = readTestData('./src/day20/input.txt');
-      const track = createTrack(data);
-      const inner = getInnerTrack(track)
-      expect(moveToEnd(track)).toBe(9324)
-      expect(inner.length).toBe(9996)
-      var count = getCounts(track, inner)
+   test("Sample 2", () => {
+      const data = readTestData("./src/day20/input.sample2.txt")
+      const track = createTrack(data)
 
-      expect (count).toBe(1346)
-    })
-  
-  })
+      const baseline =  moveToEnd(track, 0)[0]
+      recordBaseline(track, baseline.visited)
+
+      expect(getFastest(track,0)).toBe(7)
+      expect(getFastest(track,1)).toBe(5)
+      expect(getFastest(track,2)).toBe(3)
+
+      expect(getSavings(track,1,1)).toBe(1)
+   })
+
+   test("Part 1", () => {
+      const data = readTestData("./src/day20/input.txt")
+      const track = createTrack(data)
+      const baseline =  moveToEnd(track, 0)[0]
+      recordBaseline(track, baseline.visited)
+      displayTrack(track)
+      expect(getSavings(track,1,100)).toBe(1346)
+   })
+
+   test("Part 2 - Sample 2", () => {
+      const data = readTestData("./src/day20/input.sample.txt")
+      const track = createTrack(data)
+      const baseline = moveToEnd(track, 0)[0]
+
+      recordBaseline(track, baseline.visited)
+      expect(getSavings(track,20,72)).toBe(29)
+   })
+
+})
--- src/day20/day20.ts
+++ src/day20/day20.ts
@@ -1,138 +1,260 @@
 
 export interface Cell {
     content : string,
-    minVisited : number,
+    stepsLeft?: number,
+}
+
+export interface Track{
+    cells: Cell[][],
+    start: Pos,
+    end: Pos,
+    baseline?: number
 }
 
 export interface Route {
     pos: Pos,
+    steps : number,
+    cheatCount: number,
     cheatStart?: Pos,
-    cheatEnd?: Pos, 
-    cheatCount? : number
+    cheatEnd?: Pos,
+    visited: string[]
 }
 
-export const createTrack = (data: string[]) : Cell[][] =>{
+export const createTrack = (data: string[]) : Track =>{
     const results: Cell[][] = []
     for (let y = 0; y < data.length; y++) {
         const row: Cell[] = []
         for (let x = 0; x < data[y].length; x++) {
             row.push( {
                 content: data[y].charAt(x),
-                minVisited: Number.MAX_VALUE
             })
         }
         results.push(row)
     }
-    return results;
+
+    return {
+        cells: results,
+        start: getStart(results),
+        end: getEnd(results)
+    };
 }
 
-export const getStart = (track: Cell[][]) : Pos =>{
-    return track.scan (it => it.content == "S")[0]
+export const getStart = (cells: Cell[][]) : Pos =>{
+    return cells.scan (it => it.content == "S")[0]
 }
 
-export const getEnd = (track: Cell[][]) : Pos =>{
-    return track.scan (it => it.content == "E")[0]
+export const getEnd = (cells: Cell[][]) : Pos =>{
+    return cells.scan (it => it.content == "E")[0]
 }
 
-
-export const getCell = (track: Cell[][], pos: Pos): Cell  =>{
-    return track[pos.y][pos.x]
+export const getCell = (track: Track, pos: Pos): Cell  =>{
+    return track.cells[pos.y][pos.x]
 }
 
-export const canVisit = (cell: Cell, steps: number): boolean =>{
-    if (cell.minVisited< steps){
+export const canVisit = (track: Track, cell: Cell, route: Route,maxCheats: number): boolean =>{
+    const {pos} = route;
+    if (route.visited.includes(pos.x + ":"+ pos.y)){
         return false;
     }
-    return cell.content == "." || cell.content == "E";
+    if (pos.x ==0 || pos.y==0){
+        return false;
+    }
+    if (pos.x == track.cells[0].length-1){
+        return false;
+    }
+    if (pos.y == track.cells.length-1){
+        return false;
+    }
+    if (cell.content == "." || cell.content == "E"){
+        if (route.cheatStart!= undefined && route.cheatEnd == undefined){
+            route.cheatEnd = pos
+            route.steps = route.steps + cell.stepsLeft!;
+            route.pos = track.end
+        }
+        route.visited.push(pos.x + ":"+ pos.y)
+        return true;
+    }
+    // if already cheated
+    if (route.cheatEnd){
+        return false;
+    }
+    if (route.cheatCount< maxCheats){
+        route.cheatCount++;
+        if (route.cheatStart== undefined){
+            route.cheatStart = {
+                x: Number(route.visited.last()?.substringBefore(":")),
+                y: Number(route.visited.last()?.substringAfter(":")),
+            }
+        }
+        route.visited.push(pos.x + ":"+ pos.y)
+        return true;
+    }
+    return false
 }
 
-export const moveCell = (track: Cell[][], steps: number, route: Route, pos: Pos, result: Route[]) =>{
-    const cell = getCell(track, pos)
-    if (canVisit(cell, steps)){
-        cell.minVisited = steps
+export const moveCell = (track: Track, route: Route, result: Route[], maxCheats: number) =>{
+    const cell = getCell(track, route.pos)
+    if (canVisit(track, cell, route, maxCheats)){
         result.push({
             ...route,
-            pos: pos
+            steps : route.steps + 1,
         })
     }
 }
 
-export const move = (memory: Cell[][], route: Route, steps: number): Route[] =>{
+export const moveRoute = (track: Track, route: Route, maxCheats: number): Route[] =>{
     const next: Route[] = []
 
-    const up: Pos = {
-        x: route.pos.x,
-        y: route.pos.y - 1
+    const up: Route = {
+        ...route,
+        visited: [...route.visited],
+        pos: {
+            x: route.pos.x,
+            y: route.pos.y - 1
+        }
     }
-    moveCell(memory, steps, route, up, next,)
+    moveCell(track, up, next, maxCheats)
 
-    const down: Pos = {
-        x: route.pos.x,
-        y: route.pos.y + 1
+    const down: Route = {
+        ...route,
+        visited: [...route.visited],
+        pos: {
+            x: route.pos.x,
+            y: route.pos.y + 1
+        }
     }
-    moveCell(memory, steps,route, down, next)
+    moveCell(track, down, next, maxCheats)
 
-    const left: Pos = {
-        x: route.pos.x - 1,
-        y: route.pos.y
+    const left: Route = {
+        ...route,
+        visited: [...route.visited],
+        pos: {
+            x: route.pos.x - 1,
+            y: route.pos.y
+        }
     }
-    moveCell(memory, steps, route, left, next)
+    moveCell(track, left, next, maxCheats)
 
-    const right: Pos = {
-        x: route.pos.x + 1,
-        y: route.pos.y
+    const right: Route = {
+        ...route,
+        visited: [...route.visited],
+        pos: {
+            x: route.pos.x + 1,
+            y: route.pos.y
+        }
     }
-    moveCell(memory, steps, route, right, next)
+    moveCell(track, right, next, maxCheats)
     return next
 }
 
-export const moveStep = (track: Cell[][], routes: Route[], steps: number): Route[] =>{
-    return routes.flatMap ( it=> move(track, it, steps))
+export const getFastest = (track: Track, maxCheats: number): number =>{
+    const finishers = moveToEnd(track, maxCheats)
+    return finishers.minOf( it => it.steps)
 }
 
-export const moveToEnd = (track: Cell[][]): number =>{
-    track.flat().forEach (it=> it.minVisited = Number.MAX_VALUE)
-    let count = 0
-    const startRoute = {
-        pos: getStart(track)
+export const getSavings = (track: Track, maxCheats: number, saving: number): number =>{
+    const cheats = getCheats(track, maxCheats, saving)
+    const groupedSavings = new Map<number, number>()
+    for (const cheat of cheats) {
+        const current = groupedSavings.get(cheat.saving);
+        if (current== undefined){
+            groupedSavings.set(cheat.saving, 1)
+        } else{
+            groupedSavings.set(cheat.saving, current+1)
+        }
+    }
+    const ordered = groupedSavings.sort((a, b) => a[0] - b[0])
+    console.log(ordered)
+    return cheats.length
+}
+
+export interface Cheat{
+    cheatStart: Pos,
+    cheatEnd: Pos,
+    saving: number
+}
+
+export const getCheats = (track: Track, maxCheats: number, saving: number): Cheat[] =>{
+    const startRoute : Route  = {
+        pos: track.start,
+        steps: 0,
+        cheatCount: 0,
+        visited: [track.start.x + ":"+ track.start.y]
     }
     let routes = [startRoute]
-    let end = getEnd(track)
-    let steps = 0
-    while (!routes.find (it => it.pos.x ==end.x && it.pos.y==end.y)){
-        steps++
-        routes = moveStep(track, routes, steps)
-        count++
+    const cheats: Cheat[] = []
+    while (routes.length>0){
+        routes = routes.flatMap ( route=> moveRoute(track, route, maxCheats))
+        // record a cheat
+        const fin = routes.filter (it => it.pos.x ==track.end.x && it.pos.y==track.end.y)
+        for (const route of fin) {
+            if (route.cheatStart){
+                const cheatSaving = track.baseline! - route.steps
+                if (cheatSaving>= saving) {
+                    let currentRecord = cheats.find(cheat => cheat.cheatStart.x == route.cheatStart!.x && cheat.cheatStart.y == route.cheatStart!.y
+                       && cheat.cheatEnd.x == route.cheatEnd!.x && cheat.cheatEnd.y == route.cheatEnd!.y)
+                    if (currentRecord== undefined) {
+                        cheats.push({
+                            cheatStart: route.cheatStart,
+                            cheatEnd: route.cheatEnd!,
+                            saving: cheatSaving
+                        })
+                    } else{
+                        if (currentRecord.saving< cheatSaving){
+                            currentRecord.saving = cheatSaving
+                        }
+                    }
+                }
+            }
+        }
+        routes = routes.filter( it => !(it.pos.x == track.end.x && it.pos.y == track.end.y))
     }
-    return count;
+    return cheats;
 }
 
-export const getInnerTrack = ( track: Cell[][]) : Pos[] =>{
-    return track.scan( item => item.content=="#")
-       .filter (it => it.x>0 && it.y>0 && it.x< track[0].length-1 && it.y < track.length-1)
+export const moveToEnd = (track: Track, maxCheats: number): Route[] =>{
+    const startRoute : Route  = {
+        pos: track.start,
+        steps: 0,
+        cheatCount: 0,
+        visited: [track.start.x + ":"+ track.start.y]
+    }
+    let routes = [startRoute]
+    const finishers: Route[] = []
+    while (routes.length>0){
+        routes = routes.flatMap ( route=> moveRoute(track, route, maxCheats))
+        finishers.push( ...routes.filter (it => it.pos.x ==track.end.x && it.pos.y==track.end.y) )
+        routes = routes.filter( it => !(it.pos.x == track.end.x && it.pos.y == track.end.y))
+    }
+    return finishers;
 }
 
-export const getCounts = (track: Cell[][], inner: Pos[]) : number=>{
-    const baseline = moveToEnd(track)
-    const result: Map<number, number> = new Map()
-    let goodOnes = 0
-    for (const pos of inner) {
-        getCell(track, pos).content = "."
-        const count = moveToEnd(track)
-        const saving = baseline - count
-
-        if (saving>= 100){
-            goodOnes++
+export const recordBaseline = (track: Track, baseline: string[]) =>{
+    var stepsLeft = baseline.length-1
+    for (const posString of baseline) {
+        const pos = {
+            x: Number(posString.substringBefore(":")),
+            y: Number(posString.substringAfter(":"))
         }
-
-        let current = result.get(saving)
-        if (current== undefined){
-            current = 0
-        }
-        result.set(saving, current+1)
-        getCell(track, pos).content = "#"
+        const cell = getCell(track, pos)
+        cell.stepsLeft = stepsLeft;
+        stepsLeft--;
     }
-    console.log(result)
-    console.log(goodOnes)
-    return goodOnes
+    track.baseline = baseline.length-1
+}
+
+export const displayTrack= (track: Track) =>{
+    const displayRow = (row: Cell[]): string => {
+        return row.map( cell => {
+            if (cell.content=="."){
+                if (cell.stepsLeft != undefined){
+                    return "-"
+                } else{
+                    return "."
+                }
+            }
+            return cell.content;
+        }).join("")
+    }
+    track.cells.forEach( it=> console.log(displayRow(it)))
 }
